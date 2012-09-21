027e566445ee0190526bad65b41081f9ea39bbd0
Miłosz Osiński <mylosz@yahoo.pl>
2012-09-21 23:10:23 +0200

    ostateczne poprawienie komentarzy + dodanie rozpoznawania avatara gravatar po emailu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def avatar_url(comment)
+    default_url = "http://astropolis.xaa.pl/images/avatars/gallery/avatar.png"
+    gravatar_id = Digest::MD5::hexdigest(comment.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?d=#{CGI.escape(default_url)}"
+  end
 end
