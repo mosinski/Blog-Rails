0692fe5bd8e4fd96580b150e81125b2f1f52e207
Miłosz Osiński <mylosz@yahoo.pl>
2012-09-22 00:16:34 +0200

    ostatnia dziś poprawka

--- config/routes.rb
+++ config/routes.rb
@@ -59,5 +59,5 @@ end
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-root :to => "articles#start"
+root :to => "articles#index"
 end
