371c3c8908f292a33e1e5c3843011996900fabc0
Miłosz Osiński <mylosz@yahoo.pl>
2012-10-06 00:31:31 +0200

    poprawka helpera title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     gravatar_id = Digest::MD5::hexdigest(comment.email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png?d=#{CGI.escape(default_url)}"
   end
+
+  def title(page_title)
+  content_for(:title) { page_title }
+  end
 end
