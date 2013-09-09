bd600e20310fc565cff236d6db4b87aec54a9017
Miłosz Osiński <mylosz@yahoo.pl>
2013-09-09 23:32:20 +0200

    poprawka

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,6 @@ class ArticlesController < ApplicationController
   # GET /articles.json
   def index
     @articles = Article.page(params[:page]).per_page(5).search(params[:search], params[:page])
-    @articles.each {|r| r.body = coderay(r.body) }
     
     respond_to do |format|
       format.html # index.html.erb
