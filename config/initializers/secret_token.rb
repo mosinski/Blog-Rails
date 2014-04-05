7528a7fcd9dc54563b5fc645c8aa8e4726a9699c
Miłosz Osiński <mylosz@yahoo.pl>
2014-04-05 21:26:25 +0200

    clean up inicjalizer

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = 'd3ebbe4b75e8973555fe4972e27042a712686f749d4fabad06dfb1aec66cb828385713870caf523f4bfdec20a8394a1fbb96ca252d78079c2c8f66de6916faca'
+Blog::Application.config.secret_token = ENV['SECRET_TOKEN'] || ('x' * 30)
