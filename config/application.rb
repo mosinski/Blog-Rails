e02bc0cce1714638c6dc11f614a711c6a0d08e67
Miłosz Osiński <mylosz@yahoo.pl>
2014-04-09 16:39:43 +0200

    użecie tłumaczeń w widoku i ukrycie ostrzeżenia o locales

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Blog
     config.active_record.whitelist_attributes = true
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :pl
+    I18n.enforce_available_locales = false
     config.assets.enabled = true
     config.assets.version = '1.0'
     config.time_zone = 'Warsaw'
