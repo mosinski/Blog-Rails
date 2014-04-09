8f1e9564c36489b888eaac4a805f0d4eea8d2bcd
Miłosz Osiński <mylosz@yahoo.pl>
2014-04-09 15:44:02 +0200

    rozwiązanie problemów background dla precompiled assets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
   def category_counter(link_text, link_path)
     link_full = "/articles?utf8=%E2%9C%93&search=#{link_path}"
-    count = Article.find_all_by_kategoria("#{link_path}").count
+    count = Article.where(:kategoria => link_path).count
     content_tag(:li) do
       content_tag(:a, link_text, href: link_full, "dir" => "ltr") +
       content_tag(:span, "dir"=> "ltr") do
--- app/models/article.rb
+++ app/models/article.rb
@@ -7,17 +7,16 @@ class Article < ActiveRecord::Base
   has_one :visit, :as => :visitable
   has_many :comments, :dependent => :destroy
 
-def self.search(search, page)
-  paginate :per_page => 5, :page => page,
-           :conditions => ['title like :q or body like :q or kategoria like :q', q: "%#{search}%"], :order => 'created_at DESC'
-end
+  def self.search(search, page)
+    paginate :per_page => 5, :page => page,
+             :conditions => ['title like :q or body like :q or kategoria like :q', q: "%#{search}%"], :order => 'created_at DESC'
+  end
 
-def previous_article
-  self.class.first(:conditions => ["created_at < ?", created_at], :order => "created_at desc")
-end
+  def previous_article
+    self.class.first(:conditions => ["created_at < ?", created_at], :order => "created_at desc")
+  end
 
-def next_article
-  self.class.first(:conditions => ["created_at > ?", created_at], :order => "created_at asc")
-end
-
+  def next_article
+    self.class.first(:conditions => ["created_at > ?", created_at], :order => "created_at asc")
+  end
 end
