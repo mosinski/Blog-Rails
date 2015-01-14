f9f96228df50b97312e072106f472c62ed2f865f
Miłosz Osiński <mylosz@yahoo.pl>
2015-01-14 19:11:51 +0000

    resolve heroku production configuration update to rails5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,11 +4,12 @@ Blog::Application.configure do
   config.active_record.migration_error = :page_load
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.serve_static_assets = false
+  config.serve_static_files = false
   config.assets.compress = true
   config.assets.compile = false
   config.assets.digest = true
   config.log_tags = [ :uuid ]
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
+  config.log_level = :info
 end
